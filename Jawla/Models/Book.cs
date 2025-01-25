008cc2e75431b20ce2c594933c1f3d9ea084e731
Manar586 <bose63357@gmail.com>
2025-01-25 07:28:45 +0200

    first step ربنا يستر

---
+++ Jawla/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace Jawla.Models
+{
+    public class Book
+    {
+        
+        public int UserId { get; set; }
+
+        public int TripId { get; set; }
+
+        public DateTime BookingDate { get; set; } = DateTime.UtcNow;
+
+     /// <summary>
+     /// ///
+     /// </summary>
+        public User User { get; set; }
+
+        public Trip Trip { get; set; }
+    }
+}
---
+++ Jawla/Models/Favorite.cs
@@ -0,0 +1,16 @@
+﻿namespace Jawla.Models
+{
+    public class Favorite
+    {
+        public int UserId { get; set; }
+        public int TripId { get; set; }
+        ////////
+        /// <summary>
+        /// 
+        /// </summary>
+        public User User { get; set; }
+
+        public Trip Trip { get; set; }
+
+    }
+}
