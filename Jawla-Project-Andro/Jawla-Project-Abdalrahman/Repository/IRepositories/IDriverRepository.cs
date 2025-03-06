087e5be2d8b236369d690890d608a85301c32ad6
abdalrahman-abolfotoh <abwalftwhbdalrhmn50@gmail.com>
2025-03-07 00:50:21 +0300

    Revert "Delete Jawla-Project-Andro/Jawla-Project-Abdalrahman directory"
    This reverts commit 9ff0b91a45721ccfefc925e943be3753707d71bc.

---
+++ Jawla-Project-Andro/Jawla-Project-Abdalrahman/Repository/IRepositories/IDriverRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Models;
+
+namespace Repository.IRepositories
+{
+    public interface IDriverRepository: IGenericRepository<Driver, int>
+    {
+    }
+}
---
+++ Jawla-Project-Andro/Jawla-Project-Abdalrahman/Repository/Repository/DriverRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AppContext = Infrastructure.AppContext;
+using Infrastructure.Models;
+
+namespace Repository.Repository
+{
+    public class DriverRepository:GenericRepository<Driver,int>
+    {
+
+        private readonly AppContext _context;
+
+        public DriverRepository(AppContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
